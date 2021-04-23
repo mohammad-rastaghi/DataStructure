3675a715d236c4a505bded85b4fde1934a66ba24
mohammad <mohammadamin7003@gmail.com>
2021-04-23 01:31:12 -0700

    add insertEnd function

--- singly_linkedlist.py
+++ singly_linkedlist.py
@@ -6,4 +6,16 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+    
+    def insertEnd(self, newNode):
+        if self.head is None:
+            self.head = newNode
+            return
+        last_node = self.head
+        while True:
+            if last_node is None:
+                last_node = newNode
+                break
+            last_node = last_node.next
+    
+    
